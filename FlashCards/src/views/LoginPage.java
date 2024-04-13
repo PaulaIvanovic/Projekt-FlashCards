d8ccf19aa01b32159c31ce1f778732b70c70015b
Mia2404 <miazu@LegionMia>
2024-04-13 17:23:51 +0200

    Edited login page according to the gui

--- FlashCards/src/views/LoginPage.java
+++ FlashCards/src/views/LoginPage.java
@@ -1,109 +1,110 @@
 package views;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.Color;
 import javax.swing.JTextField;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPasswordField;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+
 public class LoginPage extends JFrame {
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private JTextField user;
-	private JPasswordField pass;
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-			//ako postoji frame zatvori ga
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
+    private JTextField user;
+    private JPasswordField pass;
 
-				try {
-					LoginPage frame = new LoginPage();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-	/**
-	 * Create the frame.
-	 */
-	public LoginPage() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 804, 534);
-		contentPane = new JPanel();
-		contentPane.setBackground(new Color(0, 0, 128));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(1, 89, 254));
-		panel.setBounds(10, 10, 770, 477);
-		contentPane.add(panel);
-		panel.setLayout(null);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(new Color(128, 207, 255));
-		panel_1.setBounds(220, 0, 550, 477);
-		panel.add(panel_1);
-		panel_1.setLayout(null);
-		
-		user = new JTextField();
-		user.setBounds(175, 138, 239, 19);
-		panel_1.add(user);
-		user.setColumns(10);
-		
-		JButton btnLogin = new JButton("Login");
-		btnLogin.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String un = user.getText();
-				String pas = pass.getText();
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    LoginPage frame = new LoginPage();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-				if(un.equals("admin") && pass.equals("123")) {
-					JOptionPane.showMessageDialog(null, "Login successful");
-				}else {
-					JOptionPane.showMessageDialog(null, "Invalid Username or Password");
-				}
-			}
-		});
-		btnLogin.setBounds(329, 236, 85, 21);
-		panel_1.add(btnLogin);
-		
-		pass = new JPasswordField();
-		pass.setBounds(175, 180, 239, 19);
-		panel_1.add(pass);
-		
-		JLabel lblNewLabel = new JLabel("Username:");
-		lblNewLabel.setBounds(25, 138, 85, 19);
-		panel_1.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("Password:");
-		lblNewLabel_1.setBounds(25, 183, 85, 13);
-		panel_1.add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_2 = new JLabel("Do not have an account?");
-		lblNewLabel_2.setForeground(new Color(255, 0, 0));
-		lblNewLabel_2.setBounds(51, 294, 205, 13);
-		panel_1.add(lblNewLabel_2);
+    public LoginPage() {
+        setTitle("LOGIN PAGE");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 800, 530);
+        contentPane = new JPanel();
+        contentPane.setBackground(new Color(69, 62, 130));
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
 
-		JButton btnNewButton = new JButton("Register");
-		btnNewButton.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-				RegisterPage register = new RegisterPage();
-				register.setVisible(true);
-				dispose();
-			}
-		});
-		btnNewButton.setBounds(329, 290, 85, 21);
-		panel_1.add(btnNewButton);
-	}
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(69, 62, 130));
+        panel.setBounds(188, 29, 496, 464);
+        contentPane.add(panel);
+        panel.setLayout(null);
+
+        user = new JTextField();
+        user.setBounds(82, 195, 273, 19);
+        panel.add(user);
+        user.setColumns(10);
+
+        JButton btnLogin = new JButton("Login");
+        btnLogin.setBounds(270, 319, 85, 21);
+        panel.add(btnLogin);
+
+        pass = new JPasswordField();
+        pass.setBounds(82, 246, 273, 19);
+        panel.add(pass);
+
+        JLabel usernameText = new JLabel("Username:");
+        usernameText.setFont(new Font("Tahoma", Font.BOLD, 12));
+        usernameText.setForeground(Color.WHITE);
+        usernameText.setBounds(82, 161, 160, 25);
+        panel.add(usernameText);
+
+        JLabel textpass = new JLabel("Password:");
+        textpass.setFont(new Font("Tahoma", Font.BOLD, 12));
+        textpass.setForeground(Color.WHITE);
+        textpass.setBounds(82, 224, 85, 13);
+        panel.add(textpass);
+
+        JLabel lblNewLabel_2 = new JLabel("Do not hane an account?");
+        lblNewLabel_2.setForeground(new Color(255, 0, 0));
+        lblNewLabel_2.setBounds(47, 396, 205, 13);
+        panel.add(lblNewLabel_2);
+
+        JButton btnNewButton = new JButton("Register");
+        btnNewButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                RegisterPage register = new RegisterPage();
+                register.setVisible(true);
+                dispose();
+            }
+        });
+        btnNewButton.setBounds(270, 388, 85, 21);
+        panel.add(btnNewButton);
+
+        JLabel lblNewLabel_3 = new JLabel("FLASH CARDS");
+        lblNewLabel_3.setForeground(new Color(255, 255, 255));
+        lblNewLabel_3.setFont(new Font("Nirmala UI", Font.BOLD, 54));
+        lblNewLabel_3.setBounds(112, 30, 374, 81);
+        panel.add(lblNewLabel_3);
+
+        JLabel photoLabel = new JLabel("");
+        Image IMG = new ImageIcon(this.getClass().getResource("/logo.png")).getImage().getScaledInstance(90, 90,Image.SCALE_DEFAULT);
+        photoLabel.setIcon(new ImageIcon(IMG));
+        photoLabel.setBounds(10, 30, 78, 81);
+        panel.add(photoLabel);
+
+        btnLogin.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+    }
 }
