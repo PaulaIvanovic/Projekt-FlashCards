17deecfed4abaccf755dd413eace6dedb20e5886
bazon8e82 <nikabazon2002@gmail.com>
2024-04-16 18:17:54 +0200

    Added buttons with icons. Needs editing the buttons and text fields, adding photos for icons.

--- FlashCards/src/views/Settings.java
+++ FlashCards/src/views/Settings.java
@@ -1,4 +1,3 @@
-package views;
 
 
 import java.awt.EventQueue;
@@ -11,17 +10,28 @@ import javax.swing.JButton;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.Dimension;
+
 import javax.swing.JTextField;
 import java.awt.Component;
+import javax.swing.BoxLayout;
+import java.awt.GridLayout;
+import java.awt.Image;
+
+import javax.swing.GroupLayout;
+import javax.swing.ImageIcon;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.FlowLayout;
 
 
-public class Settings extends JFrame implements GlobalDesign{
+public class Settings extends JFrame implements Template{
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JPanel scButtons;
 	private JTextField textField;
 	private JTextField textField_1;
+	
 
 	/**
 	 * Launch the application.
@@ -43,8 +53,8 @@ public class Settings extends JFrame implements GlobalDesign{
 	 * Create the frame.
 	 */
 	public Settings() {
-		setExtendedState(JFrame.MAXIMIZED_BOTH);
 		setTitle("SETTINGS");
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		contentPane = new JPanel();
@@ -54,9 +64,42 @@ public class Settings extends JFrame implements GlobalDesign{
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout());
 		
-		scButtons = new JPanel();
+		 // Create panel for round buttons with images
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 20)); // Use FlowLayout for center alignment
+        buttonPanel.setOpaque(false); // Make buttonPanel transparent
+        contentPane.add(buttonPanel, BorderLayout.NORTH);
+
+        // Array of image paths 
+        String[] imagePaths = {
+            "/nika.png",
+            "/slika.jpg",
+            "/slika.jpg",
+            "/slika.jpg",
+            "/slika.jpg",
+            "/slika.jpg"
+        };
+
+        int buttonSize = 80; // Size of each round button
+
+        for (int i = 0; i < Math.min(imagePaths.length, 6); i++) {
+            ImageIcon icon = new ImageIcon(getClass().getResource(imagePaths[i]));
+            Image img = icon.getImage().getScaledInstance(buttonSize, buttonSize, Image.SCALE_SMOOTH);
+            ImageIcon scaledIcon = new ImageIcon(img);
+
+            JButton roundButton = new JButton(scaledIcon);
+            roundButton.setPreferredSize(new Dimension(buttonSize, buttonSize)); // Set button size
+            roundButton.setBorderPainted(false); // Remove button border
+            roundButton.setContentAreaFilled(false); // Remove default button background
+            buttonPanel.add(roundButton);
+        }
+		
+		JPanel scButtons = new JPanel();
 		scButtons.setBackground(backgroundColor);
-		//scButtons.setLayout(new BorderLayout());
+		scButtons.setLayout(new BorderLayout());
+		
+		JPanel buttons = new JPanel();
+		buttons.setBackground(backgroundColor);
+		buttons.setLayout(new FlowLayout(FlowLayout.RIGHT));
 		
 		JLabel lblUsername = new JLabel("Username:");
 		lblUsername.setForeground(new Color(255, 255, 255));
@@ -79,7 +122,6 @@ public class Settings extends JFrame implements GlobalDesign{
 		changeUsername.setBackground(new Color(248, 248, 255));
 		contentPane.add(changeUsername);
 		
-
 		JLabel lblEmail = new JLabel("Email:");
 		lblEmail.setForeground(new Color(255, 255, 255));
 		lblEmail.setFont(mainFont);
@@ -99,37 +141,35 @@ public class Settings extends JFrame implements GlobalDesign{
 		
 		//button change password
 		JButton changePassword = new JButton("Change password");
+		changePassword.setPreferredSize(new Dimension(63, 21));
+		changePassword.setMinimumSize(new Dimension(63, 21));
+		changePassword.setMaximumSize(new Dimension(63, 21));
 		changePassword.setForeground(Color.BLACK);
 		changePassword.setBackground(new Color(248, 248, 255));
 		changePassword.setFont(secFont);
 		changePassword.setBounds(36, 446, 203, 33);
-		contentPane.add(changePassword);
-				
+		changePassword.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		contentPane.add(changePassword, BorderLayout.SOUTH);
+		
 		//button cancel
 		JButton cancel = new JButton("Cancel");
 		cancel.setForeground(Color.BLACK);
 		cancel.setBackground(new Color(248, 248, 255));
 		cancel.setFont(secFont);
-		cancel.setBounds(592, 513, 98, 33);
-		scButtons.add(cancel);
-				
+		
 		//button save changes
 		JButton save = new JButton("Save changes");
 		save.setForeground(Color.BLACK);
 		save.setBackground(new Color(248, 248, 255));
 		save.setFont(secFont);
-		save.setBounds(713, 513, 159, 33);
-		scButtons.add(save);
-				
-		JLabel lblDefaultSlika = new JLabel("default slika");
-		//tu nesto ne dela
-		/* Image defaultSlika = new ImageIcon(this.getClass().getResource("default.png")).getImage();
-		lblDefaultSlika.setIcon(new ImageIcon(defaultSlika)); */
-		lblDefaultSlika.setBounds(36, 49, 81, 102);
-		contentPane.add(lblDefaultSlika);
 		
-		contentPane.add(scButtons,BorderLayout.SOUTH);
+		buttons.add(cancel);
+		buttons.add(save);
+		
 	
+	
+		scButtons.add(buttons, BorderLayout.EAST);
+		contentPane.add(scButtons, BorderLayout.SOUTH);
 	
 	}
 }
