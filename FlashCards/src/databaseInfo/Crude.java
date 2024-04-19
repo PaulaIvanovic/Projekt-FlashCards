79e84b5e12a5a880f9e192f5375c1eb2c3846bf2
Mihael Bedi <mihaelbedi1@gmail.com>
2024-04-19 19:14:08 +0200

    Add - skeleton functions for CRUDE operations on the tables

--- FlashCards/src/databaseInfo/Crude.java
+++ FlashCards/src/databaseInfo/Crude.java
@@ -1,5 +1,27 @@
-package databaseInfo;
+package databaseinfo;
+
+import java.sql.ResultSet;
 
 public class Crude {
-
+	
+	public void create(String tableName, String d1, String d2, String d3) { 
+	
+	}
+	
+	public void create(String tableName, String d1, String d2, String d3, String d4) { 
+		
+	}
+	
+	
+	public void delete(String tableName, String d1) { 
+		
+	}
+	
+	
+	public void update(String tableName, String before, String after) { 
+		
+	}
+	
+	
+	
 }
