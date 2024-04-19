d950cf1561e687d18df58092a9404c4a00df567e
Mihael Bedi <mihaelbedi1@gmail.com>
2024-04-19 23:08:39 +0200

    Add
    Skeleton functions for further development, fixed JRE library version,
    fixed server connectivity

--- FlashCards/src/databaseInfo/Help.java
+++ FlashCards/src/databaseInfo/Help.java
@@ -1,5 +1,13 @@
 package databaseInfo;
 
 public class Help {
+	
+	public String encrypt(String pw) {
+		
+	}
+	
+	public String decrypt(String epw) {
+		
+	}
 
 }
--- FlashCards/src/databaseInfo/PullFrom.java
+++ FlashCards/src/databaseInfo/PullFrom.java
@@ -1,105 +1,84 @@
 package databaseInfo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSetMetaData;
+
+/*
+ * u buildpath mora bit mysql connector.jar skinut s neta i stavljen u tomcat - lib folder
+ * https://dev.mysql.com/downloads/connector/j/
+ * */
+//
+
 public class PullFrom {
-	package flashCards;
-	import java.security.NoSuchAlgorithmException;  
-	import java.security.MessageDigest;  
-	import java.sql.Connection;
-	import java.sql.DriverManager;
-	import java.sql.ResultSet;
-	import java.sql.SQLException;
-	import java.sql.Statement;
-	import java.sql.ResultSetMetaData;
-
-
-	public class PullFrom {
-		
-		public static void main(String []args) {
-			 String url = "jdbc:mysql://localhost:3306/flashcards";
-			 Connection connection = null;
-			 Statement st = null; //korististi Prepared statement za izbjegavanje sql injectiona (login,reg,edit etc.)
-			 String query = "SELECT * from user";
-			 ResultSet rs = null;
-			 ResultSetMetaData rsmd = null;
-			 try {
-				Class.forName("com.mysql.cj.jdbc.Driver");
-				try {
-					 connection = DriverManager.getConnection(url);
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			 Integer empId = -1;
-			 String firstName = null;
-			 try {
-				 st = connection.createStatement(); //izvrsavanje upita
-				 rs =  st.executeQuery(query);
-				 rsmd = rs.getMetaData(); //pomoc kod dohvata retka, stupca...
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			 //manipuliraj podacima
+	
+	public static void main(String []args) {
+		 String url = "jdbc:mysql://sql11.freemysqlhosting.net:3306/sql11700330";
+		 Connection connection = null;
+		 Statement st = null; //use Prepared statement for SQLInjection defense
+		 String query = "SELECT * from user";
+		 ResultSet rs = null;
+		 ResultSetMetaData rsmd = null;
+		 try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			try {
-				while(rs.next()) {
-				System.out.println("id: " + rs.getInt("iduser") +"\n"+
-				"ime: "+ rs.getString("username")+"\n"+"email: "+ rs.getString("email")+'\n');
-				}
-				
-				
-				
+				 connection = DriverManager.getConnection(url, "sql11700330", "ugqkTnHrnj");
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			 
-			 
-		    /* Plain-text password initialization. */  
-	        String password = "myPassword";  
-	        String encryptedpassword = null;  
-	        try   
-	        {  
-	            /* MessageDigest instance for MD5. */  
-	            MessageDigest m = MessageDigest.getInstance("MD5");  
-	              
-	            /* Add plain-text password bytes to digest using MD5 update() method. */  
-	            m.update(password.getBytes());  
-	              
-	            /* Convert the hash value into bytes */   
-	            byte[] bytes = m.digest();  
-	              
-	            /* The bytes array has bytes in decimal form. Converting it into hexadecimal format. */  
-	            StringBuilder s = new StringBuilder();  
-	            for(int i=0; i< bytes.length ;i++)  
-	            {  
-	                s.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));  
-	            }  
-	              
-	            /* Complete hashed password in hexadecimal format */  
-	            encryptedpassword = s.toString();  
-	        }   
-	        catch (NoSuchAlgorithmException e)   
-	        {  
-	            e.printStackTrace();  
-	        }  
-	          
-	        /* Display the unencrypted and encrypted passwords. */  
-	        System.out.println("Plain-text password: " + password);  
-	        System.out.println("Encrypted password using MD5: " + encryptedpassword);  
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 Integer empId = -1;
+		 String firstName = null;
+		 try {
+			 st = connection.createStatement();
+			 rs =  st.executeQuery(query);
+			 rsmd = rs.getMetaData(); //extra help with manipulating data 
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 //manipuliraj podacima
+		try {
+			while(rs.next()) {
+			System.out.println("id: " + rs.getInt("iduser") +"\n"+
+			"ime: "+ rs.getString("username")+"\n"+"email: "+ rs.getString("email")+'\n');
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+		/* 
+		public ResultSet users() {
 			
-			 
-			 try {
-				 rs.close();
-				connection.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		 }
-	}
-
+		}
+		
+		public ResultSet cards() {
+			
+		}
+		public ResultSet groups() {
+			
+		}
+		public ResultSet subgroups() {
+			
+		}*/
+		
+		
+		
+		 
+		 try {
+			 rs.close();
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	 }
 }
