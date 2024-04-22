74eec9b1b5ed3fb7d74d05aebe9581a599d06007
Mihael Bedi <mihaelbedi1@gmail.com>
2024-04-22 13:01:16 +0200

    Add - insert functionality, with testing in Help.java

--- FlashCards/src/databaseInfo/Crude.java
+++ FlashCards/src/databaseInfo/Crude.java
@@ -1,15 +1,79 @@
-package databaseinfo;
+package databaseInfo;
 
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.ResultSetMetaData;
+
+
 
 public class Crude {
 	
-	public void create(String tableName, String d1, String d2, String d3) { 
+	 String url = "jdbc:mysql://sql11.freemysqlhosting.net:3306/sql11700330";
+	 Connection connection = null;
+	 Statement st = null; //use Prepared statement for SQLInjection defense
+	 String query = null;
+	 ResultSet rs = null;
+	 ResultSetMetaData rsmd = null;
+	 
+	 
+	 public Crude() {
+		 try {
+			 Class.forName("com.mysql.cj.jdbc.Driver");
+			 connection = DriverManager.getConnection(url, "sql11700330", "ugqkTnHrnj");
+			 st = connection.createStatement();
+		 } catch (ClassNotFoundException | SQLException e) {
+			 // TODO Auto-generated catch block
+			 e.printStackTrace();
+		 }
+	 }
+
+	 //create user normalno, sto ako je boja null? 
+	public void create(String tableName, String d1, String d2, String d3, String d4) {  //
+		 
+		if(tableName.equals("user")) {
+			//d1 je username, d2 je mail, d3 pw
+			this.query = "INSERT INTO user (username, email, password, profilePic) " +
+	                "VALUES ('" + d1 + "', '" + d2 + "', '" + d3 + "', '" + d4 + "');";
+			try {
+				rs = st.executeQuery(query);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			 
+			
+		}
+		if(tableName.equals("card")) {
+			
+			this.query = "INSERT INTO user (title, paragraph, idsubgroup, boja) " +
+	                "VALUES ('" + d1 + "', '" + d2 + "', '" + d3 + "', '" + d4 + "');";
+			 
+			
+		}
 	
 	}
 	
-	public void create(String tableName, String d1, String d2, String d3, String d4) { 
-		//
+	public void create(String tableName, String d1, String d2, String d3) { 
+	//subgroup name boja idgruop
+		
+		
+		if(tableName.equals("subgroup")) {
+			//d1 je username, d2 je mail, d3 pw
+			this.query = "INSERT INTO subgroup (name, boja, idgroup) " +
+	                "VALUES ('" + d1 + "', '" + d2 + "', '" + d3 + "');"; //kartica mora pripadati nekoj grupi
+		}
+		
+	//group name iduser boja
+		if(tableName.equals("grupa")) {
+			//d1 je username, d2 je mail, d3 pw
+			if(d2 == null) {this.query = "INSERT INTO grupa (name,boja) " + //ako kartica ne pripada nijednom uzeru
+	                "VALUES ('" + d1 + "', '" + d2 + "', '" + d3 + "');";}
+			this.query = "INSERT INTO grupa (name, iduser, boja) " +
+	                "VALUES ('" + d1 + "', '" + d2 + "', '" + d3 + "');";
+		}
 	}
 	
 	
@@ -22,6 +86,14 @@ public class Crude {
 		
 	}
 	
+	public void closeConnection() {
+		try {
+			this.rs.close();
+			this.connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	
-	
+	}
 }
--- FlashCards/src/databaseInfo/Help.java
+++ FlashCards/src/databaseInfo/Help.java
@@ -3,11 +3,16 @@ package databaseInfo;
 public class Help {
 	
 	public String encrypt(String pw) {
-		
+		return "";
 	}
 	
 	public String decrypt(String epw) {
-		
+		return "";
+	}
+	
+	public static void main(String args[]) {
+		Crude crude = new Crude();
+		crude.create("user","Marko","mrakomail@mail.com","1231234","nema");
 	}
 
 }
