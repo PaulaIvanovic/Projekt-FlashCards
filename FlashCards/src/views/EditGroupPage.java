17e07f405325bdcc2a6fefe8500f95cdf3a4cd4e
HanaDzin <dzinhana@hotmail.com>
2024-04-29 16:06:52 +0200

    Base design of EditGroupPage made.

---
+++ FlashCards/src/views/EditGroupPage.java
@@ -0,0 +1,436 @@
+package views;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
+
+
+
+public class EditGroupPage extends JFrame implements GlobalDesign {
+	private static final long serialVersionUID = 1L;
+	
+	private final int HORIZONTAL_GAP_PERCENTAGE = 2;
+	
+    private final int MAX_RECTANGLE_HEIGHT = 110;
+    private final int MAX_RECTANGLE_WIDTH = 260;
+	
+	private JPanel contentPane;
+   
+	public int START_X;
+	public int START_Y;
+    
+    int windowWidth;
+    int windowHeight;
+    int xPositionWindow;
+    int yPositionWindow;
+    
+    ScreenDimensions screenSize;
+	int minimumWindowWidth;
+	int minimumWindowHeight;
+	
+	int previousWidth;
+	int previousHeight;
+    
+    public EditGroupPage() {
+    	this(0,0,0,0);
+    }
+    
+    public EditGroupPage(int x, int y, int width, int height) {
+    	//set icon for app
+    	java.net.URL IconURL = getClass().getResource("Pictures/AppIcon.png");
+	    ImageIcon Icon = new ImageIcon(IconURL);
+		setIconImage(Icon.getImage());
+
+    	screenSize = new ScreenDimensions();
+    	
+    	this.setTitle("EDIT GROUPS OF CARDS PAGE");
+    	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	
+    	this.setExtendedState(JFrame.MAXIMIZED_BOTH);		//window starts with maximum size
+    	this.setMinimumSize(new Dimension(screenSize.minimumWindowWidth, screenSize.minimumWindowHeight));
+		
+    	checkBounds(x, y, width, height);
+    	this.setBounds(xPositionWindow, yPositionWindow, windowWidth, windowHeight);
+    	updateView();
+
+    	//function for resizing components
+    	addComponentListener(new ComponentAdapter() {
+    	    public void componentResized(ComponentEvent e) {
+    	        Dimension newSize = e.getComponent().getSize();
+    	          	       
+    	        if (windowWidth != newSize.width || windowHeight != newSize.height) {
+    	        	if(newSize.width <= dimensions.minimumWindowWidth && newSize.height <= dimensions.minimumWindowHeight) {
+    	        		windowWidth = dimensions.minimumWindowWidth;
+        	            windowHeight = dimensions.minimumWindowHeight;
+    	        	} else if (newSize.width <= dimensions.minimumWindowWidth && newSize.height > dimensions.minimumWindowHeight) {
+    	        		windowWidth = dimensions.minimumWindowWidth;
+        	            windowHeight = newSize.height;
+    	        	} else if (newSize.height <= dimensions.minimumWindowHeight && newSize.width > dimensions.minimumWindowWidth) {
+    	        		windowWidth = newSize.width;
+        	            windowHeight = dimensions.minimumWindowHeight;
+    	        	} else if (newSize.width == dimensions.screenWidth && newSize.height == dimensions.screenHeight) {
+    	        		windowWidth = dimensions.screenWidth;
+    	        		windowHeight = dimensions.screenHeight;
+    	        	} else if (newSize.width == dimensions.screenWidth && newSize.height != dimensions.screenHeight) {
+    	        		windowWidth = dimensions.screenWidth;
+    	        		windowHeight = newSize.height;
+    	        	} else if (newSize.width != dimensions.screenWidth && newSize.height == dimensions.screenHeight) {
+    	        		windowWidth = newSize.width;
+    	        		windowHeight = dimensions.screenHeight;
+    	        	} else {
+    	        		windowWidth = newSize.width;
+    	        		windowHeight = newSize.height;
+    	        	}
+    	            updateView();   
+    	        }
+    	    }
+    	});
+    	
+
+    	// Listener for window state changes
+        addWindowStateListener(new WindowAdapter() {
+            @Override
+            public void windowStateChanged(WindowEvent e) {
+                if ((e.getNewState() & JFrame.MAXIMIZED_BOTH) == 0) {
+                    // Store the previous size when the window is not maximized
+                    previousWidth = getWidth();
+                    previousHeight = getHeight();
+                }
+            }
+        });
+        
+    	//when minimize/maximize button is clicked, 
+        //window goes to 50% of its original (fullscreen) size
+    	this. addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                if ((getExtendedState() & JFrame.MAXIMIZED_BOTH) == 0) {
+                    setSize(previousWidth / 2, previousHeight / 2);
+                    
+                    revalidate();
+                    repaint();
+                }
+            }
+        });
+  
+
+    	//listener for window state changes
+    	addWindowStateListener(new WindowAdapter() {
+    	    public void windowStateChanged(WindowEvent e) {
+    	        if ((e.getNewState() & JFrame.MAXIMIZED_BOTH) != 0) {
+    	            updateView();
+    	        }else if (e.getNewState() == JFrame.NORMAL) {
+    	            updateView();
+    	        }
+    	    }
+    	});
+    	
+    	//check if moved
+		addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentMoved(ComponentEvent e) {
+        		xPositionWindow = e.getComponent().getX();
+        		yPositionWindow = e.getComponent().getY();
+            }
+        });	    			
+    }
+
+    public void windowCreate() {
+    	setVisible(true);
+    	
+        windowWidth = getWidth();
+        windowHeight = getHeight();
+        views.WindowElementResize.getFontForWindowSize(windowHeight);
+
+        //main panel
+        contentPane = new JPanel();
+        contentPane.setBackground(backgroundColor);		
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout());
+		
+		//toolbar on top of the window:
+		JPanel toolbarPanel = new JPanel(new BorderLayout());
+		toolbarPanel.setBackground(toolbarColor);
+		toolbarPanel.setBounds(0, 0, windowWidth, 80);
+		toolbarPanel.setLayout(new BorderLayout());
+		toolbarPanel.setBorder(BorderFactory.createEmptyBorder(1, 10, 1, 10));
+		contentPane.add(toolbarPanel, BorderLayout.NORTH);
+		
+		//toolbar label (name of page)
+		JLabel mainTitleLabel = new JLabel("Groups of cards - edit");      
+		mainTitleLabel.setFont(WindowElementResize.mainFont);
+		mainTitleLabel.setForeground(Color.WHITE);
+		toolbarPanel.add(mainTitleLabel, BorderLayout.WEST);
+		
+		// panel with all required buttons
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setOpaque(false);
+		FlowLayout flowLayout = new FlowLayout(FlowLayout.RIGHT);
+		flowLayout.setHgap(15); 
+		buttonPanel.setLayout(flowLayout);
+		
+		//variables for button dimensions
+		int buttonDimension = (int) (windowWidth * 0.025);
+		int biggerButtonDimension = (int) (windowWidth * 0.035);
+
+		RoundedButton cancelButton = new RoundedButton("Cancel");
+		cancelButton.setPreferredSize(new Dimension(95, 40));
+		buttonPanel.add(cancelButton);
+		
+		RoundedButton saveButton = new RoundedButton("Save all");
+		saveButton.setPreferredSize(new Dimension(95, 40));
+		buttonPanel.add(saveButton);
+		
+		//edit button in toolbar
+		RoundButton editButton = new RoundButton("",buttonDimension ,buttonDimension );
+		editButton.setBackground(toolbarColor);
+		editButton.setButtonIcon("icons/EditIcon.png", buttonDimension, buttonDimension);
+		editButton.setBorder(null);
+		buttonPanel.add(editButton);
+
+		//add new group button in toolbar
+		RoundButton addGroupButton = new RoundButton("",  buttonDimension, buttonDimension);
+		addGroupButton.setButtonIcon("icons/addIcon.png",  buttonDimension, buttonDimension);
+		addGroupButton.setBackground(toolbarColor);
+		addGroupButton.setForeground(backgroundColor);
+		addGroupButton.setBorder(null);
+		buttonPanel.add(addGroupButton);
+		
+		//settings button in toolbar
+		RoundButton settingsButton = new RoundButton("", buttonDimension,buttonDimension);
+		settingsButton.setButtonIcon("icons/settingsIcon.png", buttonDimension,buttonDimension);
+		settingsButton.setBackground(toolbarColor);
+		settingsButton.setForeground(backgroundColor);
+		settingsButton.setBorder(null);
+		buttonPanel.add(settingsButton);
+		
+		settingsButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Settings settingsWindow = new Settings(xPositionWindow, yPositionWindow, windowWidth, windowHeight);
+				settingsWindow.setVisible(true);
+				dispose();
+			}
+		});
+		
+		//user icon / button in toolbar
+		RoundButton userIcon = new RoundButton("", biggerButtonDimension, biggerButtonDimension);
+		userIcon.setButtonIcon("icons/UserIconBasic.png", biggerButtonDimension, biggerButtonDimension);
+		userIcon.setBackground(toolbarColor);
+		userIcon.setBorder(null);
+		buttonPanel.add(userIcon);
+		
+		toolbarPanel.add(buttonPanel, BorderLayout.EAST);
+		
+		JScrollPane scrollPane = new JScrollPane(editGroupsCollection());	
+		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.getVerticalScrollBar().setUnitIncrement(16);
+
+        contentPane.add(scrollPane, BorderLayout.CENTER);
+    }
+		
+    public JPanel editGroupsCollection() {
+        JPanel editGroupsPanel = new JPanel(new GridBagLayout()); // main panel (all groups)
+        editGroupsPanel.setBackground(backgroundColor);
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(10, 10, 10, 10);
+
+        int numCols = Math.min(groupNames.length, 2);
+        int colCounter = 0;
+        
+        for (int i = 0; i < groupNames.length; i++) {
+            JPanel groupPanel = createGroupPanel(groupNames[i], groupColors[i]);
+            editGroupsPanel.add(groupPanel, gbc);
+            gbc.gridx++;
+            colCounter++;
+            
+            // if maximum number of columns reached, move to next row
+            if (colCounter == numCols) {
+                colCounter = 0;
+                gbc.gridx = 0;
+                gbc.gridy++;
+            }
+        }
+
+        return editGroupsPanel;
+    }
+
+    // Method to create a panel for a group
+    private JPanel createGroupPanel(String groupName, Color groupColor) {
+    	
+    	int frameWidth = windowWidth;
+        int frameHeight = windowHeight;
+        int horizontalGap = (int) (frameWidth * HORIZONTAL_GAP_PERCENTAGE / 100.0);
+
+        //calculates the number of rows and columns for the current page
+        int numCols = Math.min(groupNames.length, 3);
+        int numRows = (numCols - 1) / 3 + 1;
+    	
+    	int rectangleWidth = Math.min((frameWidth - START_X * 2 - (numCols - 1) * horizontalGap) / numCols, MAX_RECTANGLE_WIDTH);
+        int availableHeight = frameHeight - (int)(frameHeight * 0.2);
+        int rectangleHeight = Math.min(availableHeight / numRows, MAX_RECTANGLE_HEIGHT);
+    	
+        JPanel groupPanel = new JPanel(new BorderLayout()); // panel for 1 group, divided into 2 sides
+        groupPanel.setBackground(backgroundColor);
+
+        // rectangle representing group
+        RoundedButton groupOfCards = new RoundedButton(groupName);
+        groupOfCards.setBackground(groupColor);
+        groupOfCards.setFont(WindowElementResize.mediumFont);
+        groupOfCards.setPreferredSize(new Dimension(rectangleWidth, rectangleHeight));;
+        groupPanel.add(groupOfCards, BorderLayout.WEST);
+
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+        rightPanel.setBackground(backgroundColor);
+        rightPanel.setBorder(new EmptyBorder(5, 15, 5, 5));
+
+        JLabel groupNameLabel = new JLabel("Group name:");
+        groupNameLabel.setForeground(Color.WHITE);
+        groupNameLabel.setFont(smallFont);
+        rightPanel.add(groupNameLabel);
+
+        RoundTextField enterGroupName = new RoundTextField(0);
+        enterGroupName.setColumns(25);
+        enterGroupName.setText("Enter the new name of group");
+        enterGroupName.setForeground(Color.GRAY);
+        enterGroupName.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10)); // padding
+        rightPanel.add(enterGroupName);
+
+        JLabel chooseColorLabel = new JLabel("Choose a color:");
+        chooseColorLabel.setForeground(Color.WHITE);
+        chooseColorLabel.setFont(smallFont);
+        rightPanel.add(chooseColorLabel);
+
+        JPanel circlePanel = new JPanel(new GridLayout(2, 5, 5, 5));
+        circlePanel.setBackground(backgroundColor);
+
+        for (int i = 0; i < 16; i++) {
+            RoundButton circle = new RoundButton("");
+            circle.setBackground(groupColors[i]);
+            circle.setPreferredSize(new Dimension(30, 30));
+            circle.setBorder(null);
+            circlePanel.add(circle);
+        }
+
+        // click on circle = change of group color
+        for (Component component : circlePanel.getComponents()) {
+            if (component instanceof RoundButton) {
+                RoundButton circleButton = (RoundButton) component;
+                circleButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        Color buttonColor = circleButton.getBackground();
+                        groupOfCards.setBackground(buttonColor);
+                    }
+                });
+            }
+        }
+
+        rightPanel.add(circlePanel);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setBackground(backgroundColor);
+
+        RoundedButton saveChangesButton = new RoundedButton("Save");
+        saveChangesButton.setPreferredSize(new Dimension(80, 30));
+        buttonPanel.add(saveChangesButton);
+
+        RoundedButton deleteGroupButton = new RoundedButton("Delete");
+        deleteGroupButton.setPreferredSize(new Dimension(80, 30));
+        buttonPanel.add(deleteGroupButton);
+
+        rightPanel.add(buttonPanel);
+
+        groupPanel.add(rightPanel, BorderLayout.CENTER);
+
+        return groupPanel;
+    }
+    
+    
+	//updates sizes of elements and window
+	public void updateView() {
+		 START_X = (int)(windowWidth * 0.05);
+		 START_Y = (int)(windowHeight * 0.175); 
+		windowCreate();
+	}
+	
+	//function for checking bounds
+	public void checkBounds(int x, int y, int width, int height) {
+		//with tolerances
+		if(x <= -10 || y <= -10) {
+			xPositionWindow = 0;
+			yPositionWindow = 0;
+		}else {
+			xPositionWindow = x;
+			yPositionWindow = y;
+		}
+		
+		if(width <= 0 || height <= 0) { //if the screen is less then the minimum allowed size
+			//with tolerances
+			windowWidth = dimensions.screenWidth;
+			windowHeight = dimensions.screenHeight;
+			setExtendedState(JFrame.MAXIMIZED_BOTH);
+		}else if(width <= dimensions.minimumWindowWidth && height <= dimensions.minimumWindowHeight) {
+			windowWidth = dimensions.minimumWindowWidth;
+			windowHeight = dimensions.minimumWindowHeight;
+		}else if(width <= dimensions.minimumWindowWidth && height > dimensions.minimumWindowHeight) {
+    		windowWidth = dimensions.minimumWindowWidth;
+            windowHeight = height;
+    	}else if(height <= dimensions.minimumWindowHeight && width > dimensions.minimumWindowWidth) {
+    		windowWidth = width;
+            windowHeight = dimensions.minimumWindowHeight;
+    	}else {
+			windowWidth = width;
+			windowHeight = height;
+		}
+	
+		
+		//with tolerances
+				if (x <= -10 || y <= -10) {
+					xPositionWindow = 0;
+					yPositionWindow = 0;
+				} else {
+					xPositionWindow = x;
+					yPositionWindow = y;
+				}
+	}
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    EditGroupPage frame = new EditGroupPage(0, 0, 0, 0);
+                    frame.setVisible(true);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
