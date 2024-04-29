8c7396a0833ca88daecde2a4ccb3a96edecd4e40
Paula Ivanović <paulaivanovic6@gmail.com>
2024-04-29 12:55:07 +0200

    Remove duplicate main

--- FlashCards/src/databaseInfo/PullFrom.java
+++ FlashCards/src/databaseInfo/PullFrom.java
@@ -72,12 +72,6 @@ public class PullFrom {
         }
         return infoList;
     }
-    public static void main(String args[]) {
-        PullFrom p = new PullFrom("grupa","iduser", "2");
-        ResultSet res;
-        System.out.println(p.returnInfo("iduser"));
-        p.close();
-     }
 	
 	//this need to be called every time we use PullFrom
 	//closing the rs connection
